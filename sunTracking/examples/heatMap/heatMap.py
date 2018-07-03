2a60da2d932b7976a74983661b9cfc4c708eba49
Bose <bose@iMac.local>
2018-07-03 21:53:27 +0700

    revert

--- sunTracking/examples/heatMap/heatMap.py
+++ sunTracking/examples/heatMap/heatMap.py
@@ -1,10 +1,8 @@
-import plotly
+import plotly.plotly as py
 import plotly.graph_objs as go
 
 trace = go.Heatmap(z=[[1, 20, 30],
                       [20, 1, 60],
                       [30, 60, 1]])
 data=[trace]
-
-plotly.offline.init_notebook_mode(connected=True)
-plotly.offline.iplot(data, filename='basic-heatmap', include_plotlyjs = false)
+py.iplot(data, filename='basic-heatmap')
