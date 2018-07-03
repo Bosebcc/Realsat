3599dd4628b3ff5c4ab426924eaf340b0ed4e42f
Bose <bose@iMac.local>
2018-07-03 21:48:35 +0700

    Heatmap test with reduce filesize

--- sunTracking/examples/heatMap/heatMap.py
+++ sunTracking/examples/heatMap/heatMap.py
@@ -5,5 +5,4 @@ trace = go.Heatmap(z=[[1, 20, 30],
                       [20, 1, 60],
                       [30, 60, 1]])
 data=[trace]
-fig2plotly(gcf, 'offline', true, 'include_plotlyjs', false, 'filename', 'sine wave'
-py.iplot(data, filename='basic-heatmap')
+py.iplot(data, filename='basic-heatmap', include_plotlyjs = false)
