49dea6d4c998b46bdcc964a897779ee07dfa655c
Bose <bose@iMac.local>
2018-07-03 16:41:17 +0700

    Syntax

--- sunTracking/sources/stepper.py
+++ sunTracking/sources/stepper.py
@@ -152,8 +152,12 @@ if __name__ == "__main__":
     for x in range(11):
         y = x*36
         m.move_to(y)
-    for a in range(11)
+        sleep(0.2)
+    for a in range(11):
         b = 360 - (a*36)
+        m.move_to(b)
+        sleep(0.2)
+        '''
     m.move_to(180)
     sleep(1)
     m.move_to(0)
@@ -162,4 +166,5 @@ if __name__ == "__main__":
     m.move_to(180)
     sleep(1)
     m.move_to(0)
+    '''
     GPIO.cleanup()
