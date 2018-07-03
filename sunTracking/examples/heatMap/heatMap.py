e7d95cfe5392b4ec6247be24d20ceb4c6d797451
Bose <bose@iMac.local>
2018-07-03 21:49:45 +0700

    Re

--- sunTracking/examples/heatMap/heatMap.py
+++ sunTracking/examples/heatMap/heatMap.py
@@ -5,4 +5,6 @@ trace = go.Heatmap(z=[[1, 20, 30],
                       [20, 1, 60],
                       [30, 60, 1]])
 data=[trace]
-py.iplot(data, filename='basic-heatmap', include_plotlyjs = false)
+
+plotly.offline.init_notebook_mode(connected=True)
+plotly.offline.iplot(data, filename='basic-heatmap', include_plotlyjs = false)
