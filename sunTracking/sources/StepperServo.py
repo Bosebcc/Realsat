506e4059f54dcb23c8cbcabe89ec3fcc76079413
Bose <bose@iMac.local>
2018-07-03 14:39:12 +0700

    Fix 1

--- sunTracking/sources/StepperServo.py
+++ sunTracking/sources/StepperServo.py
@@ -25,49 +25,45 @@ for pin in control_pins:
   GPIO.setup(pin, GPIO.OUT)
   GPIO.output(pin, 0)
 halfstep_seq = [
-  [1,0,0,0],
-  [1,1,0,0],
-  [0,1,0,0],
-  [0,1,1,0],
-  [0,0,1,0],
-  [0,0,1,1],
-  [0,0,0,1],
-  [1,0,0,1]
+  (1, 0, 0, 0),
+  (0, 1, 0, 0),
+  (0, 0, 1, 0),
+  (0, 0, 0, 1)
 ]
 for i in range(512):
   for halfstep in range(8):
     for pin in range(4):
       GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
     time.sleep(0.001)
-    for x in range(21):
-        pulse = (x * 100)+500   #turn  servo 100 pulse from 500-2500
-        pi.set_servo_pulsewidth(gpioServo, pulse)
-        print(servoPos)
-        time.sleep(0.1)
-        vis = sensor.readVisible()
-        IR = sensor.readIR()
-        UV = sensor.readUV()
-        uvIndex = UV / 100.0
-        if highVisible < uvIndex:
-            servoPos = x
-            highVisible = uvIndex
-            pass
-        print('SunLight Sensor read at time: %s' % datetime.now())
-        print '		Vis:             ' + str(vis)
-        print '		IR:              ' + str(IR)
-        print '		UV Index:        ' + str(uvIndex)
+  for x in range(21):
+      pulse = (x * 100)+500   #turn  servo 100 pulse from 500-2500
+      pi.set_servo_pulsewidth(gpioServo, pulse)
+      print(servoPos)
+      time.sleep(0.025)
+      vis = sensor.readVisible()
+      IR = sensor.readIR()
+      UV = sensor.readUV()
+      uvIndex = UV / 100.0
+      if highVisible < uvIndex:
+          servoPos = x
+          highVisible = uvIndex
+          pass
+      print('SunLight Sensor read at time: %s' % datetime.now())
+      print '		Vis:             ' + str(vis)
+      print '		IR:              ' + str(IR)
+      print '		UV Index:        ' + str(uvIndex)
 
-        if uvIndex <= 3 :
-            print "Warning:" + "Wear Sun Glass; Low UV"
-        elif uvIndex > 3 and uvIndex <= 6 :
-            print "Warning:" + "Take cover when avalible; Moderate UV"
-        elif uvIndex > 6 and uvIndex >= 8 :
-            print "Warning:" + "Apply SPF 30+ sunscreen, don't stay out more than 3 hours; High UV"
-        elif uvIndex > 8 and uvIndex >= 11 :
-            print "Warning:" + "Do not stay in the sun for too long; Very High UV"
-        else :
-            print "Warning:" + "Take all Percautions; Extreme UV"
-        pass
+      if uvIndex <= 3 :
+          print "Warning:" + "Wear Sun Glass; Low UV"
+      elif uvIndex > 3 and uvIndex <= 6 :
+          print "Warning:" + "Take cover when avalible; Moderate UV"
+      elif uvIndex > 6 and uvIndex >= 8 :
+          print "Warning:" + "Apply SPF 30+ sunscreen, don't stay out more than 3 hours; High UV"
+      elif uvIndex > 8 and uvIndex >= 11 :
+          print "Warning:" + "Do not stay in the sun for too long; Very High UV"
+      else :
+          print "Warning:" + "Take all Percautions; Extreme UV"
+      pass
 GPIO.cleanup()
 
 
