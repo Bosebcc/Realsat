b6671f14ddf87dd67d7a2328f91049bf020361e7
Bhumsitt Pramuanpornsatid <Bosebcc@Hotmail.com>
2018-07-05 15:18:16 +0700

    Implement multithreading to master

--- Threading/simpleThreading.py
+++ Threading/simpleThreading.py
@@ -1,11 +1,19 @@
 import threading
 import time
 
+x = 0
+
 class sunTracking(threading.Thread):
     print ("Starting sunTracking")
 
-thread1 = sunTracking()
+def repeat():
+    while(True):
+        print ("Repeat")
+        time.sleep(1)
 
+thread1 = sunTracking()
+thread2 = threading.Timer(1, repeat)
 thread1.start()
+thread2.start()
 
 print ("Exiting Main Thread")
