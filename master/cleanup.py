14a80402f7530286ad2da8719fb5771fa7b82d6c
Bose <bose@iMac.local>
2018-07-07 18:07:13 +0700

    add stepper

--- master/cleanup.py
+++ master/cleanup.py
@@ -32,5 +32,115 @@ class Motor(object):
             GPIO.setup(p, GPIO.OUT)
             GPIO.output(p, 0)
 
+    def _set_rpm(self, rpm):
+        """Set the turn speed in RPM."""
+        self._rpm = rpm
+        # T is the amount of time to stop between signals
+        self._T = (60.0 / rpm) / self.steps_per_rev
+
+    # This means you can set "rpm" as if it is an attribute and
+    # behind the scenes it sets the _T attribute
+    rpm = property(lambda self: self._rpm, _set_rpm)
+
+    def move_to(self, angle):
+        """Take the shortest route to a particular angle (degrees)."""
+        # Make sure there is a 1:1 mapping between angle and stepper angle
+        target_step_angle = 8 * (int(angle / self.deg_per_step) / 8)
+        steps = target_step_angle - self.step_angle
+        steps = (steps % self.steps_per_rev)
+        if steps > self.steps_per_rev / 2:
+            steps -= self.steps_per_rev
+            print "moving " + `steps` + " steps"
+            if self.mode == 2:
+                self._move_acw_2(-steps / 8)
+            else:
+                self._move_acw_3(-steps / 8)
+        else:
+            print "moving " + `steps` + " steps"
+            if self.mode == 2:
+                self._move_cw_2(steps / 8)
+            else:
+                self._move_cw_3(steps / 8)
+        self.step_angle = target_step_angle
+
+    def __clear(self):
+        GPIO.output(self.P1, 0)
+        GPIO.output(self.P2, 0)
+        GPIO.output(self.P3, 0)
+        GPIO.output(self.P4, 0)
+
+    def _move_acw_2(self, big_steps):
+        self.__clear()
+        for i in range(big_steps):
+            GPIO.output(self.P3, 0)
+            GPIO.output(self.P1, 1)
+            sleep(self._T * 2)
+            GPIO.output(self.P2, 0)
+            GPIO.output(self.P4, 1)
+            sleep(self._T * 2)
+            GPIO.output(self.P1, 0)
+            GPIO.output(self.P3, 1)
+            sleep(self._T * 2)
+            GPIO.output(self.P4, 0)
+            GPIO.output(self.P2, 1)
+            sleep(self._T * 2)
+
+    def _move_cw_2(self, big_steps):
+        self.__clear()
+        for i in range(big_steps):
+            GPIO.output(self.P4, 0)
+            GPIO.output(self.P2, 1)
+            sleep(self._T * 2)
+            GPIO.output(self.P1, 0)
+            GPIO.output(self.P3, 1)
+            sleep(self._T * 2)
+            GPIO.output(self.P2, 0)
+            GPIO.output(self.P4, 1)
+            sleep(self._T * 2)
+            GPIO.output(self.P3, 0)
+            GPIO.output(self.P1, 1)
+            sleep(self._T * 2)
+
+    def _move_acw_3(self, big_steps):
+        self.__clear()
+        for i in range(big_steps):
+            GPIO.output(self.P1, 0)
+            sleep(self._T)
+            GPIO.output(self.P3, 1)
+            sleep(self._T)
+            GPIO.output(self.P4, 0)
+            sleep(self._T)
+            GPIO.output(self.P2, 1)
+            sleep(self._T)
+            GPIO.output(self.P3, 0)
+            sleep(self._T)
+            GPIO.output(self.P1, 1)
+            sleep(self._T)
+            GPIO.output(self.P2, 0)
+            sleep(self._T)
+            GPIO.output(self.P4, 1)
+            sleep(self._T)
+
+    def _move_cw_3(self, big_steps):
+        self.__clear()
+        for i in range(big_steps):
+            GPIO.output(self.P3, 0)
+            sleep(self._T)
+            GPIO.output(self.P1, 1)
+            sleep(self._T)
+            GPIO.output(self.P4, 0)
+            sleep(self._T)
+            GPIO.output(self.P2, 1)
+            sleep(self._T)
+            GPIO.output(self.P1, 0)
+            sleep(self._T)
+            GPIO.output(self.P3, 1)
+            sleep(self._T)
+            GPIO.output(self.P2, 0)
+            sleep(self._T)
+            GPIO.output(self.P4, 1)
+            sleep(self._T)
+
+m = Motor([6,13,19,26])
 GPIO.cleanup()
 pi.stop()
