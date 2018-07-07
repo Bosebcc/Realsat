653cc4b7d08c3e5f8bf59567c65c77fc1ac455cb
Thanakrit Tr <nattzkungz@Thanakrits-MacBook-Pro.local>
2018-07-08 00:47:47 +0700

    Storing Data #2 by nattz (use fh)

--- master/testfile.py
+++ master/testfile.py
@@ -1,5 +1,25 @@
-file = open(“testfile.txt”,”w”)
+# coding: utf-8
 
-file.write(“Hello World”) 
+fh = open("testfile.txt","w")
 
-file.close()
+fh.write("Hello World")
+fh.write("This is our new text file")
+fh.write("and this is another line")
+fh.write("Why, Because we can")
+
+x = "Hellooooooo"
+print x
+
+
+
+
+
+fh.close()
+
+file = open("testfile.txt","r")
+for line in file:
+    print line
+
+
+
+# credit : http://www.pythonforbeginners.com/files/reading-and-writing-files-in-python
