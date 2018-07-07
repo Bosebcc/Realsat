7ee7d861a2759827489cfc845ba60a30a34bc629
Bose <bose@iMac.local>
2018-07-07 17:32:41 +0700

    Add gpio cleanup

--- master/MasterSun_Servo.py
+++ master/MasterSun_Servo.py
@@ -571,10 +571,13 @@ def sunTracking():
     return state
 
 if __name__ == '__main__':
-    jobs = []
-    sun = multiprocessing.Process(target=sunTracking)
-    baro = multiprocessing.Process(target=barometer)
-    jobs.append(sun)
-    jobs.append(baro)
-    sun.start()
-    baro.start()
+    try:
+        jobs = []
+        sun = multiprocessing.Process(target=sunTracking)
+        baro = multiprocessing.Process(target=barometer)
+        jobs.append(sun)
+        jobs.append(baro)
+        sun.start()
+        baro.start()
+    except KeyboardInterrupt:
+        GPIO.cleanup()
