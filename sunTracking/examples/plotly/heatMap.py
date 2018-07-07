e935912f3469dd3188337b279b61cc9d8261fe1a
Bosebcc <32533504+Bosebcc@users.noreply.github.com>
2018-07-07 18:53:58 +0700

    Sunlight Tracking v1.1 (#24)
    * Add more delay when Servo reset
    
    * Fix Delay
    
    * Update example code
    
    * servo Test minimal
    
    * fix global variable
    
    * Add gpio cleanup as a file
    
    * Multiproccesing
    
    * multiprocessing cleanup

---
+++ sunTracking/examples/plotly/heatMap.py
@@ -0,0 +1,12 @@
+import plotly
+#from plotly.offline import download_plotlyjs, init_notebook_mode, plot,iplot
+import plotly.graph_objs as go
+
+plotly.offline.init_notebook_mode(connected=True)
+
+plotly.offline.iplot({
+    "data": [go.Scatter(z = [[1, 20, 30],
+                          [20, 1, 60],
+                          [30, 60, 1]])],
+    "layout": go.Layout(title="basic-heatmap")
+})
