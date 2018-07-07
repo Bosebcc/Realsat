73ad408632d080117da5eae7df62bfbc57997d4f
Bose <bose@iMac.local>
2018-07-07 11:58:09 +0700

    Stop baro after suntracking is finish

--- master/MasterSun_Servo.py
+++ master/MasterSun_Servo.py
@@ -338,15 +338,14 @@ def barometer():
    s = BME280.sensor(pi)
 
    stop = time.time() + 60
-   while (state == True):
+   if state == True:
     while stop > time.time():
        t, p, h = s.read_data()
        getAltitude = ((math.pow((sea_press / (p/100.0)), 1/5.257) - 1.0) * (t + 273.15)) / 0.0065; #Pressure to Altitude Equation
        print("h={:.2f} p={:.2f} t={:.2f} Alt={:.1f}".format(h, p/100.0, t, getAltitude)) #:.2f set decimal to 2 places
        time.sleep(0.9)
-
+    else
     s.cancel()
-    print "%s: %s" % ( threadName, time.ctime(time.time()) )
     return
 
 class Motor(object):
