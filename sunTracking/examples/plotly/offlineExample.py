09a8eba828b9e19c60471d46d6eb45e47a7e792c
Bose <bose@iMac.local>
2018-07-09 18:40:31 +0700

    offline plot test

--- sunTracking/examples/plotly/offlineExample.py
+++ sunTracking/examples/plotly/offlineExample.py
@@ -1,9 +1,7 @@
-import plotly.HTTP_PORT
+import plotly
 import plotly.graph_objs as go
 
-import numpy as np
-
-x = np.random.randn(2000)
-y = np.random.randn(2000)
-iplot([go.Histogram2dContour(x=x, y=y, contours=dict(coloring='heatmap')),
-       go.Scatter(x=x, y=y, mode='markers', marker=dict(color='white', size=3, opacity=0.3))], show_link=False)
+plotly.offline.plot({
+    "data": [go.Scatter(x=[1, 2, 3, 4], y=[4, 3, 2, 1])],
+    "layout": go.Layout(title="hello world")
+}, auto_open=True)
