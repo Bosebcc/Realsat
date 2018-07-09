0f59d3fbb58e276ca17da3082ab96601f8d4a261
Bose <bose@iMac.local>
2018-07-09 22:52:57 +0700

    fix var

--- master/writeFile.py
+++ master/writeFile.py
@@ -3,7 +3,7 @@ highVisible = 4.2
 
 ctime = str(time.ctime(time.time()))
 light = str(highVisible)
-f = open("sunlightdata.txt", "w")
+file = open("sunlightdata.txt", "w")
 file.write("Highest UV")
 file.write(ctime)
 file.write(light)
