7cf4be27598b822266d998a6bf4baf140d49ff3b
Bhumsitt Pramuanpornsatid <Bosebcc@Hotmail.com>
2018-07-15 18:30:47 +0700

    fix readline

--- gps/gpsWrite.py
+++ gps/gpsWrite.py
@@ -3,7 +3,7 @@ import serial
 gps = serial.Serial("/dev/ttyAMA0", baudrate = 9600)
 
 while True:
-    line = gps.readLine()
+    line = gps.readline()
     data = line.split(",")
     if data[0] == "$GPRMC":
         if data[2] == "A":
