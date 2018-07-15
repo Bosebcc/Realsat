6fcf2e883f7bd53da2150406871bddf3897d4d6e
Bose <bose@iMac.local>
2018-07-15 10:33:51 +0700

    test

--- sunTracking/examples/plotly/offlineExample.py
+++ sunTracking/examples/plotly/offlineExample.py
@@ -2,4 +2,4 @@ import plotly
 import plotly.graph_objs as go
 
 plotly.offline.plot({
-    go.Scatter(​z=[[1, 20, 30, 50, 1], [20, 1, 60, 80, 30], [30, 60, 1, -10, 20]], colorscale=[[0.0, 'rgb(​255,255,255)'], [1.0, 'rgb(​0,0,0)']])
+    go.Scatter(​z=[[1, 20, 30, 50, 1], [20, 1, 60, 80, 30], [30, 60, 1, -10, 20]])
