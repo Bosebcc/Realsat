5338608198a4d0323f405b8b229282f40f010d3f
Bose <bose@iMac.local>
2018-07-16 22:22:52 +0700

    Fix barometer displaying only one time

--- master/MasterSun_Servo.py
+++ master/MasterSun_Servo.py
@@ -341,13 +341,13 @@ def barometer():
 
     stop = time.time() + 60
     while True:
-        if stateSun:
+        if stateSun == True:
         #while stop > time.time():
             t, p, h = s.read_data()
             getAltitude = ((math.pow((sea_press / (p/100.0)), 1/5.257) - 1.0) * (t + 273.15)) / 0.0065; #Pressure to Altitude Equation
             print("h={:.2f} p={:.2f} t={:.2f} Alt={:.1f}".format(h, p/100.0, t, getAltitude)) #:.2f set decimal to 2 places
             time.sleep(1)
-        if stateSun:
+        if stateSun == False:
             s.cancel()
             break
 
