0a038206894825edf07f2e75739c81a927e4046b
Bhumsitt Pramuanpornsatid <Bosebcc@Hotmail.com>
2018-07-19 14:24:33 +0700

    0x68

--- gyro/example/BerryIMU-master/python-BerryIMU-gryo-accel-compass/IMU.py
+++ gyro/example/BerryIMU-master/python-BerryIMU-gryo-accel-compass/IMU.py
@@ -6,17 +6,17 @@ import time
 
 
 
-LSM9DS0 = 1
+LSM9DS0 = 0
 
 
 
 def detectIMU():
-    #Detect which version of BerryIMU is connected.   
+    #Detect which version of BerryIMU is connected.
     #BerryIMUv1 uses the LSM9DS0
     #BerryIMUv2 uses the LSM9DS1
     global LSM9DS0
-    
-    
+
+    '''
     try:
         #Check for LSM9DS0
         #If no LSM9DS0 is conencted, there will be an I2C bus error and the program will exit.
@@ -46,10 +46,11 @@ def detectIMU():
             LSM9DS0 = 0
 
     time.sleep(1)
+    '''
 
 
 
-    
+
 
 
 def writeAG(register,value):
@@ -123,7 +124,7 @@ def readGYRx():
         gyr_combined = (gyr_l | gyr_h <<8)
 
         return gyr_combined  if gyr_combined < 32768 else gyr_combined - 65536
-  
+
 
 def readGYRy():
         gyr_l = bus.read_byte_data(GYR_ADDRESS, OUT_Y_L_G)
@@ -153,20 +154,20 @@ def writeACC(register,value):
     else:
         bus.write_byte_data(LSM9DS1_ACC_ADDRESS , register, value)
     return -1
-    
+
 
 def writeMAG(register,value):
     if(LSM9DS0):
         bus.write_byte_data(LSM9DS0_MAG_ADDRESS, register, value)
     else:
-        bus.write_byte_data(LSM9DS1_MAG_ADDRESS , register, value)        
+        bus.write_byte_data(LSM9DS1_MAG_ADDRESS , register, value)
         return -1
 
 def writeGRY(register,value):
     if(LSM9DS0):
         bus.write_byte_data(LSM9DS0_GYR_ADDRESS, register, value)
     else:
-        bus.write_byte_data(LSM9DS1_GYR_ADDRESS , register, value)        
+        bus.write_byte_data(LSM9DS1_GYR_ADDRESS , register, value)
         return -1
 
 
@@ -189,8 +190,8 @@ def readACCy():
         acc_h = bus.read_byte_data(LSM9DS0_ACC_ADDRESS, LSM9DS0_OUT_Y_H_A)
     else:
         acc_l = bus.read_byte_data(LSM9DS1_ACC_ADDRESS, LSM9DS1_OUT_Y_L_XL)
-        acc_h = bus.read_byte_data(LSM9DS1_ACC_ADDRESS, LSM9DS1_OUT_Y_H_XL)       
-    
+        acc_h = bus.read_byte_data(LSM9DS1_ACC_ADDRESS, LSM9DS1_OUT_Y_H_XL)
+
     acc_combined = (acc_l | acc_h <<8)
     return acc_combined  if acc_combined < 32768 else acc_combined - 65536
 
@@ -202,7 +203,7 @@ def readACCz():
     else:
         acc_l = bus.read_byte_data(LSM9DS1_ACC_ADDRESS, LSM9DS1_OUT_Z_L_XL)
         acc_h = bus.read_byte_data(LSM9DS1_ACC_ADDRESS, LSM9DS1_OUT_Z_H_XL)
-	
+
     acc_combined = (acc_l | acc_h <<8)
     return acc_combined  if acc_combined < 32768 else acc_combined - 65536
 
@@ -237,7 +238,7 @@ def readMAGz():
         mag_h = bus.read_byte_data(LSM9DS0_MAG_ADDRESS, LSM9DS0_OUT_Z_H_M)
     else:
         mag_l = bus.read_byte_data(LSM9DS1_MAG_ADDRESS, LSM9DS1_OUT_Z_L_M)
-        mag_h = bus.read_byte_data(LSM9DS1_MAG_ADDRESS, LSM9DS1_OUT_Z_H_M)           
+        mag_h = bus.read_byte_data(LSM9DS1_MAG_ADDRESS, LSM9DS1_OUT_Z_H_M)
 
     mag_combined = (mag_l | mag_h <<8)
     return mag_combined  if mag_combined < 32768 else mag_combined - 65536
@@ -251,10 +252,10 @@ def readGYRx():
     else:
         gyr_l = bus.read_byte_data(LSM9DS1_GYR_ADDRESS, LSM9DS1_OUT_X_L_G)
         gyr_h = bus.read_byte_data(LSM9DS1_GYR_ADDRESS, LSM9DS1_OUT_X_H_G)
-    
+
     gyr_combined = (gyr_l | gyr_h <<8)
     return gyr_combined  if gyr_combined < 32768 else gyr_combined - 65536
-  
+
 
 def readGYRy():
     if (LSM9DS0):
@@ -302,7 +303,7 @@ def initIMU():
         #initialise the gyroscope
         writeGRY(LSM9DS1_CTRL_REG4,0b00111000)      #z, y, x axis enabled for gyro
         writeGRY(LSM9DS1_CTRL_REG1_G,0b10111000)    #Gyro ODR = 476Hz, 2000 dps
-        writeGRY(LSM9DS1_ORIENT_CFG_G,0b00111000)   #Swap orientation 
+        writeGRY(LSM9DS1_ORIENT_CFG_G,0b00111000)   #Swap orientation
 
         #initialise the accelerometer
         writeACC(LSM9DS1_CTRL_REG5_XL,0b00111000)   #z, y, x axis enabled for accelerometer
@@ -313,16 +314,3 @@ def initIMU():
         writeMAG(LSM9DS1_CTRL_REG2_M, 0b01000000)   #+/-12gauss
         writeMAG(LSM9DS1_CTRL_REG3_M, 0b00000000)   #continuos update
         writeMAG(LSM9DS1_CTRL_REG4_M, 0b00000000)   #lower power mode for Z axis
-
-
-
-
-
-
-
-
-
-
-
-
-
