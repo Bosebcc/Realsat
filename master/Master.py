e4069cd4d2b939921a3de8fe5bd20cce2432aee7
Bhumsitt Pramuanpornsatid <Bosebcc@Hotmail.com>
2018-07-24 13:42:13 +0700

    fix servoPose cal

--- master/Master.py
+++ master/Master.py
@@ -523,33 +523,8 @@ def sunTracking():
                     stepPos = degreeOfTurn
                     highVisible = uvIndex
                     pass
-                #print('SunLight Sensor read at time: %s' % datetime.now())
-                #print '		Vis:             ' + str(vis)
-                #print '		IR:              ' + str(IR)
-                #print '		UV Index:        ' + str(uvIndex)
             servoPos = (servoPos * 100)+500
-            pi.set_servo_pulsewidth(gpioServo, servoPos)
-            print(servoPos)
-            m.move_to(stepPos)
-            time.sleep(1)
-            pi.set_servo_pulsewidth(gpioServo, 0)
-            #calculating effect on human
-            uvIrradiance = highVisible * 0.025 * 60 / 10
-            print "Uv Irradiance: " + str(uvIrradiance)
-            if uvIrradiance > 2.67 :
-                print "Your skin will start to burn and tanning under 15 minutes, please find a place to hide from uv now"
-            elif uvIrradiance <= 2.67 and uvIrradiance > 1.33 :
-                print "Your skin will start to burn and tanning within 15 minutes"
-            elif uvIrradiance <= 1.33 and uvIrradiance > 0.89 :
-                print "Your skin will start to burn and tanning within half an hour"
-            elif uvIrradiance <= 0.89 and uvIrradiance > 0.67 :
-                print "Your skin will start to burn and tanning within an hour"
-            else :
-                print "Your skin will start to burn and tanning more than an hour"
-            #print("Ps. This case is for Mediterranean, Asian and Latino people only")
             servoTurnDirection == False
-            if servoTurnDirection == False:
-                break
         else:
             for x in range(21):
                 pulse = 2500-(x * 100)
@@ -578,29 +553,13 @@ def sunTracking():
                 #print '		Vis:             ' + str(vis)
                 #print '		IR:              ' + str(IR)
                 #print '		UV Index:        ' + str(uvIndex)
-            servoPos = (servoPos * 100)+500
-            pi.set_servo_pulsewidth(gpioServo, servoPos)
-            print(servoPos)
-            m.move_to(stepPos)
-            time.sleep(1)
-            pi.set_servo_pulsewidth(gpioServo, 0)
-            #calculating effect on human
-            uvIrradiance = highVisible * 0.025 * 60 / 10
-            print "Uv Irradiance: " + str(uvIrradiance)
-            if uvIrradiance > 2.67 :
-                print "Your skin will start to burn and tanning under 15 minutes, please find a place to hide from uv now"
-            elif uvIrradiance <= 2.67 and uvIrradiance > 1.33 :
-                print "Your skin will start to burn and tanning within 15 minutes"
-            elif uvIrradiance <= 1.33 and uvIrradiance > 0.89 :
-                print "Your skin will start to burn and tanning within half an hour"
-            elif uvIrradiance <= 0.89 and uvIrradiance > 0.67 :
-                print "Your skin will start to burn and tanning within an hour"
-            else :
-                print "Your skin will start to burn and tanning more than an hour"
-            #print("Ps. This case is for Mediterranean, Asian and Latino people only")
+            servoPos = 2500-(servoPos*100)
             servoTurnDirection == True
-            if servoTurnDirection == True:
-                break
+        pi.set_servo_pulsewidth(gpioServo, servoPos)
+        print(servoPos)
+        m.move_to(stepPos)
+        time.sleep(1)
+        pi.set_servo_pulsewidth(gpioServo, 0)
         stateSun = False
 
 if __name__ == '__main__':
@@ -620,6 +579,7 @@ if __name__ == '__main__':
     sun.join()
     baro.join()
 
+    '''
     #highestUV record
     file.write("_________________________________________________ \n")
     file.write("Highest UV \n")
@@ -628,6 +588,7 @@ if __name__ == '__main__':
     file.write(uvWrite)
 
     file.close()
+    '''
 
     pi.stop()
     GPIO.cleanup()
